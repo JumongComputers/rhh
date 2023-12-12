48ea76cb4bcfeca18e9e0f6afea19bada927564b
kelechi <igwesiemmanuel4@gmail.com>
2023-12-13 00:33:09 +0100

    booking done

--- src/components/LandingPage/BookRooms.tsx
+++ src/components/LandingPage/BookRooms.tsx
@@ -14,33 +14,23 @@ const BookRooms: React.FC = () => {
   }, []);
 
   return (
-    <div className="relative bg-black mb-20 lg:mb-32 ">
-      <img
-        src="/images/hero1.jpg"
-        alt="room image"
-        className="w-full bg-black opacity-25 h-full object-cover"
-      />
+    <div className="relative h-[220vh] lg:h-full bg-black mb-20 lg:mb-32 ">
+      <img src="/images/hero1.jpg" alt="room image" className="w-full bg-black opacity-25 h-full object-cover" />
       {/* content */}
       <div
-        className="absolute inset-0 flex flex-col px-8 lg:px-[8vw] gap-12 justify-center items-center text-center text-white"
+        className="absolute inset-0 flex flex-col px-6 md:px-[8%] gap-12 justify-center xl:gap-20 text-white"
         data-aos="fade-up"
         data-aos-duration="1000"
       >
-        <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-bold">Our Rooms</h1>
+        <div className="flex flex-col gap-2 lg:justify-center lg:items-center px-8 lg:px-[8vw]  text-center">
+          <h1 className="lg:text-8xl text-6xl font-bold">Our Rooms</h1>
           <p className="font-normal text-base md:text-2xl opacity-70">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            atque quia doloribus dignissimos fugit vitae perferendis! Ut
-            adipisci, laboriosam nulla aliquam vero dolore natus optio culpa in!
-            Blanditiis, quos repellat?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores atque quia doloribus dignissimos fugit vitae perferendis! Ut adipisci,
+            laboriosam nulla aliquam vero dolore natus optio culpa in! Blanditiis, quos repellat?
           </p>
         </div>
 
-        <div
-          className="grid lg:grid-cols-3 grid-cols-1 gap-12"
-          data-aos="fade-up"
-          data-aos-duration="1000"
-        >
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-8 lg:gap-12" data-aos="fade-up" data-aos-duration="1000">
           {bookingData.map((data: Booking, index: number) => (
             <div key={index} data-aos="fade-up" data-aos-duration="1000">
               <BookingCard data={data} />
--- src/components/subComponents/BookingCard.tsx
+++ src/components/subComponents/BookingCard.tsx
@@ -1,5 +1,4 @@
 import { Booking } from "@/data/bookingData";
-import Link from "next/link";
 
 interface BookingCardProps {
   data: Booking;
@@ -7,28 +6,19 @@ interface BookingCardProps {
 
 const BookingCard: React.FC<BookingCardProps> = ({ data }) => {
   return (
-    <div className="flex justify-center text-center gap-8 items-center flex-col w-full px-8 py-8 bg-white">
+    <div className="flex justify-center text-center h-[60vh] lg:h-[50vh] xl:h-[100vh] gap-8 items-center flex-col w-full px-8 py-8 bg-white">
       <img src={data.image} alt={data.name} />
-      <span className="text-[#282d3c] font-normal uppercase text-2xl">
-        {data.name}
-      </span>
-      <span className="text-[#282d3c] font-normal text-base">
-        {data.numOfRooms}
-      </span>
+      <span className="text-[#282d3c] font-normal uppercase text-2xl">{data.name}</span>
+      <span className="text-[#282d3c] font-normal text-base">{data.numOfRooms}</span>
       <span className="text-[#282d3c] font-normal text-base">{data.text}</span>
       <div className="flex gap-1 items-center text-2xl">
         <span className="text-[#282d3c] font-bold text-2xl">{data.price}</span>
-        <span className="text-[#282d3c] font-bold text-2xl text-opacity-25">
-          / per night
-        </span>
+        <span className="text-[#282d3c] font-bold text-2xl text-opacity-25">/ per night</span>
       </div>
-      <Link href="/rooms">
-        <button className="bg-blue-400 w-full rounded-full p-3">
-          <span className="text-white text-center font-bold text-base px-3">
-            Book Now
-          </span>
-        </button>
-      </Link>
+
+      <button className="bg-blue-400 w-[44vw] lg:w-[12vw] rounded-full p-3">
+        <span className="text-white text-center font-bold text-base px-3">Book Now</span>
+      </button>
     </div>
   );
 };
