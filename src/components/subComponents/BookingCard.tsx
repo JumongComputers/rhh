5d716ca956003d45fa4443ec0feb0cbf5e4c02e1
kelechi <igwesiemmanuel4@gmail.com>
2023-12-13 01:25:19 +0100

    booking modal done

--- src/components/subComponents/BookingCard.tsx
+++ src/components/subComponents/BookingCard.tsx
@@ -1,10 +1,17 @@
 import { Booking } from "@/data/bookingData";
+import BookingModal from "../Modals/BookingModal";
+import { useState } from "react";
 
 interface BookingCardProps {
   data: Booking;
 }
 
 const BookingCard: React.FC<BookingCardProps> = ({ data }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
     <div className="flex justify-center text-center h-[60vh] lg:h-[50vh] xl:h-[100vh] gap-8 items-center flex-col w-full px-8 py-8 bg-white">
       <img src={data.image} alt={data.name} />
@@ -16,9 +23,12 @@ const BookingCard: React.FC<BookingCardProps> = ({ data }) => {
         <span className="text-[#282d3c] font-bold text-2xl text-opacity-25">/ per night</span>
       </div>
 
-      <button className="bg-blue-400 w-[44vw] lg:w-[12vw] rounded-full p-3">
+      <button onClick={openModal} className="bg-blue-400 w-[44vw] lg:w-[12vw] rounded-full p-3">
         <span className="text-white text-center font-bold text-base px-3">Book Now</span>
       </button>
+
+      {/* Modal */}
+      <BookingModal isOpen={isModalOpen} onRequestClose={closeModal} />
     </div>
   );
 };
