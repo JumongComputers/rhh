bf2834a2a76e004abe29928dcda59f2abef915ea
nielvid <nielvidservices@gmail.com>
2023-12-17 18:12:47 +0100

    pr reviewed and accepted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,19 +27,6 @@ const Home: React.FC<HomeProps> = ({ data }) => {
     <Layout customClass="min-h-screen w-full max-w-full">
       <Hero />
       <AboutHero />
-
-     {/* <div className="group relative w-96">
-        <img className="w-full object-cover" src="https://www.slingacademy.com/wp-content/uploads/2022/10/example.png"  alt="Sling Academy" />
-        <div
-            className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
-            <h1 className="text-2xl text-white">Fiction T-Shirt Store</h1>
-            <a className="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300" href="#">Continue
-                Shopping</a>
-        </div>
-    </div> */}
-  
-
-
       <Grid data={data} />
 
       <BookRooms />
