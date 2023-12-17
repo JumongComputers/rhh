dd4532a7dd19fadac221dd56e9bbe9d9362aca9a
kelechi <igwesiemmanuel4@gmail.com>
2023-12-17 10:44:26 +0100

    added overlay on image for preview

--- src/components/subComponents/BookingCard.tsx
+++ src/components/subComponents/BookingCard.tsx
@@ -1,20 +1,33 @@
 import { Booking } from "@/data/bookingData";
 import BookingModal from "../Modals/BookingModal";
 import { useState } from "react";
+import { Camera } from "lucide-react";
 
 interface BookingCardProps {
   data: Booking;
 }
 
 const BookingCard: React.FC<BookingCardProps> = ({ data }) => {
+  const [isOverlayVisible, setOverlayVisible] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleImagePreview = () => setOverlayVisible(!isOverlayVisible);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
 
   return (
     <div className="flex text-center h-[75vh] sm:h-[75vh] lg:h-[50vh] xl:h-[80vh] gap-8 items-center flex-col w-full px-8 py-8 bg-white">
-      <img src={data.image} alt={data.name} />
+      <div className="relative" onMouseEnter={() => setOverlayVisible(true)} onMouseLeave={() => setOverlayVisible(false)}>
+        <img src={data.image} alt={data.name} />
+        {isOverlayVisible && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black opacity-60">
+            <div className="absolute top-1/2 left-1/2 transform cursor-pointer -translate-x-1/2 -translate-y-1/2">
+              <Camera size={24} color="#fff" onClick={toggleImagePreview} />
+            </div>
+          </div>
+        )}
+      </div>
       <span className="text-[#282d3c] font-normal uppercase text-5xl">{data.name}</span>
       <span className="text-[#282d3c] font-normal text-xl">{data.numOfRooms}</span>
       <span className="text-[#282d3c] font-normal h-[9rem] text-xl">{data.text}</span>
