6d9d414d9ea450128f8b8fe1f179dc27dc1de559
kelechi <igwesiemmanuel4@gmail.com>
2023-12-20 14:34:54 +0100

    integrated login endpoint

--- src/components/Auth-Admin/Login.tsx
+++ src/components/Auth-Admin/Login.tsx
@@ -1,36 +1,42 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Eye, EyeOff, MoveLeft } from "lucide-react";
-
-interface FormData {
-  email: string;
-  password: string;
-}
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "@/redux/slices/authSlice";
 
 const Login: React.FC = () => {
   const router = useRouter();
-  const [formData, setFormData] = useState<FormData>({
-    email: "",
-    password: "",
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state: { auth: { loading: string } }) => state.auth.loading);
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Invalid email address"),
+    password: Yup.string().required("Password is required"),
   });
-  const { email, password } = formData;
-  const [isLoading, setIsLoading] = useState(false);
 
-  const login = (e: FormEvent) => {
-    e.preventDefault();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await dispatch(userLogin(values) as any);
 
-    // Simulate loading
-    setIsLoading(true);
+        if (!isLoading) {
+          router.push("/admin");
+        }
+      } catch (error) {
+        console.error("Login error:", error);
+      }
+    },
+  });
 
-    // Simulate success
-    setTimeout(() => {
-      setIsLoading(false);
-
-      // Simulate navigation after successful login
-      router.push("/admin");
-    }, 2000);
-  };
+  const { email, password } = formik.values;
 
   const [open, setOpen] = useState(false);
 
@@ -39,13 +45,12 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="relative h-screen bg-black ">
+    <div className="relative h-screen bg-black">
       <img
         src="https://res.cloudinary.com/dyijwff8m/image/upload/v1702731461/gridex/ab2_gabvo7.webp"
         alt="room image"
         className="w-full bg-black opacity-25 h-full object-cover"
       />
-      {/* content */}
       <div className="flex flex-col inset-0 absolute items-center h-screen lg:justify-center w-full md:px-20 flex-1 md:py-20">
         <div className="bg-white h-screen rounded-lg flex lg:max-w-4xl w-full max-w-full">
           <div className=" w-full px-5 md:px-8 py-8 md:py-20">
@@ -60,7 +65,7 @@ const Login: React.FC = () => {
             </h1>
             <p className="mt-4 text-[#676869] font-normal text-lg">Enter your login details below.</p>
 
-            <form onSubmit={login} className="flex flex-col gap-4">
+            <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
               <div className="flex flex-col mt-12">
                 <label htmlFor="email" className="text-[#19202C] text-lg mb-2">
                   Email or Phone
@@ -71,12 +76,14 @@ const Login: React.FC = () => {
                   required={!email.includes("@") && !email.match(/^\d+$/)}
                   name="email"
                   value={email}
-                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   className="py-4 px-6 rounded-md bg-[#F2F7FF] focus:outline-none w-full
               placeholder-gray-200::placeholder placeholder-opacity-75
               border focus:border-[#0D60D8]"
                   placeholder="Enter your email or phone number"
                 />
+                {formik.touched.email && formik.errors.email && <span className="text-red-500">{formik.errors.email}</span>}
               </div>
               <div className="flex flex-col w-full">
                 <label htmlFor="password" className="text-[#19202C] text-lg mb-2">
@@ -89,7 +96,8 @@ const Login: React.FC = () => {
                     required
                     name="password"
                     value={password}
-                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     className="py-4 px-6 rounded-md bg-[#F2F7FF] focus:outline-none w-full
                     placeholder-gray-200::placeholder placeholder-opacity-75
                     border focus:border-[#0D60D8]"
@@ -99,6 +107,7 @@ const Login: React.FC = () => {
                     {open === false ? <Eye name="eye" onClick={toggle} size={20} /> : <EyeOff name="eye-off" onClick={toggle} size={20} />}
                   </div>
                 </div>
+                {formik.touched.password && formik.errors.password && <span className="text-red-500">{formik.errors.password}</span>}
               </div>
 
               <Link href="/auth/forgotPassword" className="text-black font-normal text-base md:text-lg">
@@ -107,11 +116,11 @@ const Login: React.FC = () => {
 
               <button
                 type="submit"
-                disabled={isLoading}
+                disabled={isLoading === "pending"}
                 className="lg:w-full bg-[#0D60D8] py-4 text-white rounded-md
               font-bold text-lg focus:outline-none w-full mt-6"
               >
-                {isLoading ? "Loading..." : "Login"}
+                {isLoading === "pending" ? "Loading..." : "Login"}
               </button>
             </form>
           </div>
